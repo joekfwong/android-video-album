94d94d5637c4049fe691aa668befd9a7b22ae510
joekfwong <kamfung.wong@yahoo.com>
2016-11-19 02:56:47 +0800

    Login screen

--- app/src/main/java/hk/hku/cs/videoalbum/MainActivity.java
+++ app/src/main/java/hk/hku/cs/videoalbum/MainActivity.java
@@ -1,7 +1,11 @@
 package hk.hku.cs.videoalbum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = ((EditText) findViewById(R.id.loginInput)).getText().toString();
+                String password = ((EditText) findViewById(R.id.passwordInput)).getText().toString();
+
+                if (username.trim().length() > 0 && password.trim().length() > 0) {
+                    // login activity
+                } else {
+                    // prompt enter both username and password
+
+                }
+            }
+        });
     }
 }
