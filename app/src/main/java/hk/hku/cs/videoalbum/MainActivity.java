decf6a38d976e627c7bd3dea72af902ef7a8ccfb
joekfwong <kamfung.wong@yahoo.com>
2016-11-23 17:35:27 +0800

    Basic login function

--- app/src/main/java/hk/hku/cs/videoalbum/MainActivity.java
+++ app/src/main/java/hk/hku/cs/videoalbum/MainActivity.java
@@ -1,11 +1,23 @@
 package hk.hku.cs.videoalbum;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +35,114 @@ public class MainActivity extends AppCompatActivity {
 
                 if (username.trim().length() > 0 && password.trim().length() > 0) {
                     // login activity
+                    connect(username, password);
                 } else {
                     // prompt enter both username and password
-
+                    alert( "Login", "Please enter username and password" );
                 }
             }
         });
     }
+
+    protected void alert(String title, String mymessage){
+        new AlertDialog.Builder(this)
+                .setMessage(mymessage)
+                .setTitle(title)
+                .setCancelable(true)
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton){}
+                        }
+                )
+                .show();
+    }
+
+    public String getJsonPage(String url) {
+        HttpURLConnection conn_cshomepage = null;
+        final int HTML_BUFFER_SIZE = 2*1024*1024;
+        char htmlBuffer[] = new char[HTML_BUFFER_SIZE];
+
+        try {
+            URL url_cshomepage = new URL(url);
+            conn_cshomepage = (HttpURLConnection) url_cshomepage.openConnection();
+            conn_cshomepage.setInstanceFollowRedirects(true);
+
+            BufferedReader reader_moodle = new BufferedReader(new InputStreamReader(conn_cshomepage.getInputStream()));
+            String HTMLSource = ReadBufferedHTML(reader_moodle, htmlBuffer, HTML_BUFFER_SIZE);
+            reader_moodle.close();
+            return HTMLSource;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Fail to login";
+        } finally {
+            // When HttpClient instance is no longer needed,
+            // shut down the connection manager to ensure
+            // immediate deallocation of all system resources
+            if (conn_cshomepage != null) {
+                conn_cshomepage.disconnect();
+            }
+        }
+    }
+
+    public String ReadBufferedHTML(BufferedReader reader, char [] htmlBuffer, int bufSz) throws java.io.IOException
+    {
+        htmlBuffer[0] = '\0';
+        int offset = 0;
+        do {
+            int cnt = reader.read(htmlBuffer, offset, bufSz - offset);
+            if (cnt > 0) {
+                offset += cnt;
+            } else {
+                break;
+            }
+        } while (true);
+        return new String(htmlBuffer);
+    }
+
+    public void connect(final String username, final String password){
+        final ProgressDialog pdialog = new ProgressDialog(this);
+
+        pdialog.setCancelable(false);
+        pdialog.setMessage("Logging in ...");
+        pdialog.show();
+
+        final String url = "http://i.cs.hku.hk/~kfwong/videoalbum/login.php?username=" + username + "&userkey=" + password;
+
+        AsyncTask<String, Void, String> task = new AsyncTask<String, Void, String>() {
+            boolean success;
+            String jsonString;
+
+            @Override
+            protected String doInBackground(String... arg0) {
+                // TODO Auto-generated method stub
+                success = true;
+                jsonString = getJsonPage(url);
+                if (jsonString.equals("Fail to login"))
+                    success = false;
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String response) {
+                if (success) {
+                    try {
+                        JSONObject rootJSONObj = new JSONObject(jsonString);
+                        String result = rootJSONObj.getString("result");
+                        if ("SUCCESS".equals(result)) {
+                            alert( "Login", "Success" );
+                        } else {
+                            alert( "Login", "Failure" );
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    alert( "Error", "Fail to login" );
+                }
+                pdialog.hide();
+            }
+        }.execute("");
+    }
+
 }
